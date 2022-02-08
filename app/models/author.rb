a9fb7c7fb7e456ba281f826eff8e58385ea8c621
Erick Contreras <eri89--@hotmail.com>
2022-02-08 15:10:50 -0600

    Add Author model with generate command

---
+++ app/models/author.rb
@@ -0,0 +1,5 @@
+class Author < ApplicationRecord
+  def name
+    "#{first_name} #{last_name}"
+  end
+end
--- test/controllers/about_controller_test.rb
+++ test/controllers/about_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class AboutControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get about_index_url
     assert_response :success
   end
