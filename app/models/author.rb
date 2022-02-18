51986fafb66b44f0bc1d979ddb451b4c9e9e0b61
Erick Contreras <eri89--@hotmail.com>
2022-02-18 16:44:29 -0600

    Add validations to book model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ApplicationRecord
   has_and_belongs_to_many :books
+
   validates :first_name, :last_name, presence: true
 
   def name
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,11 @@
 class Book < ApplicationRecord
   has_and_belongs_to_many :authors
   belongs_to :publisher
+
+  validates :title, length: { in: 1..255 }
+  validates :publisher, :authors, :published_at, presence: true
+  validates :page_count, numericality: { only_integer: true }
+  validates :price, numericality: true
+  validates :isbn, format: { with: /[0-9\-xX]{13}/ }
+  validates :isbn, uniqueness: true
 end
