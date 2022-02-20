f2f7ee45186a7369695c0d34d60ddaafd6a9b372
Erick Contreras <eri89--@hotmail.com>
2022-02-20 16:13:59 -0600

    Add test_create to book_test.rb

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -14,4 +14,19 @@ class BookTest < ActiveSupport::TestCase
     assert book.errors[:page_count]
     assert book.errors[:price]
   end
+
+  def test_create
+    book = Book.new(
+      title: 'Ruby for Toddlers',
+      publisher_id: Publisher.find(1).id,
+      published_at: Time.now,
+      authors: Author.all,
+      isbn: '123-123-123-1',
+      blurb: 'The best book since "Bodo Bär zu Hause"',
+      page_count: 12,
+      price: 40.4
+    )
+
+    assert book.save
+  end
 end
