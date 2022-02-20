f4d3bf5a206e5ba0c7cac0c0bad20690a2b59639
Erick Contreras <eri89--@hotmail.com>
2022-02-20 15:03:37 -0600

    Add test_failing_create to BookTest

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,17 @@
-require 'test_helper'
+require './test/test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_failing_create
+    book = Book.new
+    assert_equal false, book.save
+
+    assert_equal 7, book.errors.size
+    assert book.errors[:title]
+    assert book.errors[:publisher]
+    assert book.errors[:authors]
+    assert book.errors[:published_at]
+    assert book.errors[:isbn]
+    assert book.errors[:page_count]
+    assert book.errors[:price]
+  end
 end
