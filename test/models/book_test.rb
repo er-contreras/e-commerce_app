893ab45d2aae6b9b6f289d41cefcdacef97e8c88
Erick Contreras <eri89--@hotmail.com>
2022-02-28 17:17:46 -0600

    Fix typeMismatch error in book_test.rb

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -30,53 +30,54 @@ class BookTest < ActiveSupport::TestCase
     assert book.save
   end
 
-  # def test_has_many_and_belongs_to_mapping
-  #   apress = Publisher.find_by(name: 'Apress')
-  #   assert_equal 2, apress.books.size
+  # Unit Testing the One-to-Many Mapping
+  def test_has_many_and_belongs_to_mapping
+    apress = Publisher.find_by(name: 'Apress')
+    assert_equal 2, apress.books.size
 
-  #   book = Book.new(
-  #     title: 'Rails E-Commerce 3nd Edition',
-  #     # authors: [
-  #     #   Author.find_by(first_name: 'Christian', last_name: 'Hellsten'),
-  #     #   Author.find_by(first_name: 'Jarkko', last_name: 'Laine')
-  #     # ],
-  #     # authors: Author.find_by(first_name: 'Christian', last_name: 'Hellsten'),
-  #     authors: Author.all,
-  #     published_at: Time.now,
-  #     isbn: '123-123-123-x',
-  #     blurb: 'E-Commerce on Rails',
-  #     page_count: 300,
-  #     price: 30.5
-  #   )
+    book = Book.new(
+      title: 'Rails E-Commerce 3nd Edition',
+      authors: [
+        Author.find_or_create_by(first_name: 'Christian', last_name: 'Hellsten'),
+        Author.find_or_create_by(first_name: 'Jarkko', last_name: 'Laine')
+      ],
+      published_at: Time.now,
+      isbn: '123-123-123-x',
+      blurb: 'E-Commerce on Rails',
+      page_count: 300,
+      price: 30.5
+    )
 
-  #   apress.books << book
+    apress.books << book
 
-  #   apress.reload
-  #   book.reload
+    apress.reload
+    book.reload
 
-  #   assert_equal 3, apress.books.size
-  #   assert_equal 'Apress', book.publisher.name
-  # end
+    assert_equal 3, apress.books.size
+    assert_equal 'Apress', book.publisher.name
+  end
 
-  # def test_has_and_belongs_to_many_authors_mapping
-  #   book = Book.new(
-  #     title: 'Rails E-Commerce 3nd Edition',
-  #     publisher: Publisher.find_by_name('Apress'),
-  #     # authors: [Author.find_by_first_name_and_last_name('Christian', 'Hellsten'),
-  #     #           Author.find_by_first_name_and_last_name('Jarkko', 'Laine')],
-  #     # authors: Author.all,
-  #     published_at: Time.now,
-  #     isbn: '123-123-123-x',
-  #     blurb: 'E-Commerce on Rails',
-  #     page_count: 300,
-  #     price: 30.5
-  #   )
+  # Unit Testing the Many-to-Many Mapping
+  def test_has_and_belongs_to_many_authors_mapping
+    book = Book.new(
+      title: 'Rails E-Commerce 3nd Edition',
+      publisher: Publisher.find_by(name: 'Apress'),
+      authors: [
+        Author.find_or_create_by(first_name: 'Christian', last_name: 'Hellsten'),
+        Author.find_or_create_by(first_name: 'Jarkko', last_name: 'Laine')
+      ],
+      published_at: Time.now,
+      isbn: '123-123-123-x',
+      blurb: 'E-Commerce on Rails',
+      page_count: 300,
+      price: 30.5
+    )
 
-  #   assert book.save
+    assert book.save
 
-  #   book.reload
+    book.reload
 
-  #   assert_equal 2, book.authors.size
-  #   assert_equal 2, Author.find_by_first_name_and_last_name('Christian', 'Hellsten').books.size
-  # end
+    assert_equal 2, book.authors.size
+    assert_equal 1, Author.find_by(first_name: 'Christian', last_name: 'Hellsten').books.size
+  end
 end
