be71f24b6a2d0dd94dbcb2857bb95478f353e185
Erick Contreras <eri89--@hotmail.com>
2022-09-15 21:59:07 -0500

    Add remote: true

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,16 @@
 module ApplicationHelper
   def add_book_link(text, book)
-    link_to text, { url: { controller: 'cart', action: 'add', id: book } },
+    link_to text, { url: { controller: 'cart', action: 'add', id: book }, remote: true },
             { title: 'Add to cart', href: url_for(controller: 'cart', action: 'add', id: book) }
   end
 
-  # def remove_book_link(text, book)
-  #   link_to text, { url: { controller: "cart", action: "remove", id: book}},
-  #                 { title: "Remove book", href: url_for( controller: "cart", action: "remove", id: book) }
-  # end
+  def remove_book_link(text, book)
+    link_to text, { url: { controller: 'cart', action: 'remove', id: book}, remote: true },
+            { title: 'Remove book', href: url_for( controller: 'cart', action: 'remove', id: book) }
+  end
 
-  # def clear_cart_link(text = "Clear Cart")
-  #   link_to text, { url: { controller: "cart", action: "clear" } },
-  #                 { href: url_for( controller: "cart", action: "clear") }
-  # end
+  def clear_cart_link(text = 'Clear Cart')
+    link_to text, { url: { controller: 'cart', action: 'clear' }, remote: true },
+            { href: url_for( controller: 'cart', action: 'clear') }
+  end
 end
