65dc4d280233a599a9a8730e1aa3dd8497a076d8
Erick Contreras <eri89--@hotmail.com>
2022-12-08 23:07:04 -0600

    Use ajax: true

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,24 +1,39 @@
 class CartController < ApplicationController
   before_action :initialize_cart
 
-  def add
-    # binding.pry
+  # def add
+  #   # binding.pry
+  #
+  #   logger.debug "is it a xml_html_request? #{request.xhr?}\n #{request}" if request
+  #   logger.debug "is it a post request? #{request.post?}\n #{request}" if request
+  #
+  #   @book = Book.find(params[:id])
+  #
+  #   if request.xhr?
+  #     @item = @cart.add(params[:id])
+  #     flash.now[:cart_notice] = "Added from AJAX <em>#{@item.book.title}</em>"
+  #     render action: 'add_with_ajax'
+  #   elsif request.post?
+  #     @item = @cart.add(params[:id])
+  #     flash[:cart_notice] = "Added from POST #{@item.book.title}"
+  #     redirect_to session[:return_to] || { controller: 'catalog' }
+  #   else
+  #     render
+  #   end
+  # end
 
-    logger.debug "is it a xml_html_request? #{request.xhr?}\n #{request}" if request
-    logger.debug "is it a post request? #{request.post?}\n #{request}" if request
-
-    @book = Book.find(params[:id])
-
-    if request.xhr?
-      @item = @cart.add(params[:id])
-      flash.now[:cart_notice] = "Added from AJAX <em>#{@item.book.title}</em>"
-      render action: 'add_with_ajax'
-    elsif request.post?
-      @item = @cart.add(params[:id])
-      flash[:cart_notice] = "Added from POST #{@item.book.title}"
-      redirect_to session[:return_to] || { controller: 'catalog' }
-    else
-      render
-    end
-  end
+  # def add
+  #   @book = Book.new(book_params)
+  #
+  #   respond_to do |format|
+  #     @book.save
+  #     format.js
+  #   end
+  # end
+  #
+  # private
+  #
+  # def book_params
+  #   params.permit(:id)
+  # end
 end
--- app/javascript/controllers/add_with_ajax_controller.js
+++ app/javascript/controllers/add_with_ajax_controller.js
@@ -1,26 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
-import {getMetaValue} from "@rails/activestorage/src/helpers";
 
 // Connects to data-controller="add-with-ajax"
 export default class extends Controller {
-  static value = { id: String }
+  static values = { url: String }
 
-  load({ params: { url } }) {
-    fetch(url, {
-      body: 2,
-      method: 'POST',
-      dataType: 'script',
-      credentials: 'include',
-      headers: {
-        'X-CSRF-Token': getMetaValue('csrf-token')
-      },
-    })
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
+    load() {
+      console.log('Hi there', this.element)
   }
 }
