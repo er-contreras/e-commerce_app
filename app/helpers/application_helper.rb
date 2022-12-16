9d2619ab511cc3d95625229c61ac7158735e6d7c
Erick Contreras <eri89--@hotmail.com>
2022-12-15 22:57:54 -0600

    Clean code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,2 @@
 module ApplicationHelper
-  def add_book_link(text, book)
-    link_to text,
-            { url: { controller: 'cart', action: 'add', id: book } },
-            { title: 'Add to cart', class: 'add_to_cart', href: url_for(controller: 'cart', action: 'add', id: book) }
-  end
-
-  def remove_book_link(text, book)
-    link_to text, { url: { controller: 'cart', action: 'remove', id: book } },
-            { title: 'Remove book', href: url_for(controller: 'cart', action: 'remove', id: book) }
-  end
-
-  def clear_cart_link(text = 'Clear Cart')
-    link_to text, { url: { controller: 'cart', action: 'clear' } },
-            { href: url_for(controller: 'cart', action: 'clear') }
-  end
 end
